e9f9929629df034d7409194c9cc6c7071fbcc7d3
taekwan-hwang <ethan.taekwanhwang@gmail.com>
2019-05-05 20:33:28 +0900

    complete

--- csim.c
+++ csim.c
@@ -1,7 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <getopt.h>
 #include "cachelab.h"
 
-int main()
-{
-    printSummary(0, 0, 0);
-    return 0;
+
+typedef struct {
+  bool valid;
+  int tag;
+  int timestamp;
+} line_t;
+
+typedef struct {
+  line_t *lines;
+} set_t;
+
+typedef struct {
+  set_t *sets;
+  size_t set_num;
+  size_t line_num;
+} cache_t;
+
+cache_t cache = {};
+int set_bits = 0, block_bits = 0;
+size_t hits = 0, misses = 0, evictions = 0;
+
+
+void simulate(int addr);
+int main(int argc, char *argv[]) {
+  FILE *file = 0;
+  for (int opt; (opt = getopt(argc, argv, "s:E:b:t:")) != -1;) {
+    switch (opt) {
+      case 's':
+        set_bits = atoi(optarg);
+        cache.set_num = 2 << set_bits;
+        break;
+      case 'E':
+        cache.line_num = atoi(optarg);
+        break;
+      case 'b':
+        block_bits = atoi(optarg);
+        break;
+      case 't':
+        if (!(file = fopen(optarg, "r"))) { return 1; }
+        break;
+      default:
+        return 1;
+    }
+  }
+  if (!set_bits || !cache.line_num || !block_bits || !file) { return 1; }
+
+  cache.sets = malloc(sizeof(set_t) * cache.set_num);
+  for (int i = 0; i < cache.set_num; ++i) {
+    cache.sets[i].lines = calloc(sizeof(line_t), cache.line_num);
+  }
+
+  char kind;
+  int addr;
+
+  while (fscanf(file, " %c %x%*c%*d", &kind, &addr) != EOF) {
+    if (kind == 'I') { continue; }
+
+    simulate(addr);
+    if ('M' == kind) { simulate(addr); }
+  }
+  printSummary(hits, misses, evictions);
+
+  fclose(file);
+  for (size_t i = 0; i < cache.set_num; ++i) { free(cache.sets[i].lines); }
+  free(cache.sets);
+  return 0;
+}
+
+
+void update(set_t *set, size_t line_no);
+void simulate(int addr) {
+  size_t set_index = (0x7fffffff >> (31 - set_bits)) & (addr >> block_bits);
+  int tag = 0xffffffff & (addr >> (set_bits + block_bits));
+
+  set_t *set = &cache.sets[set_index];
+
+  for (size_t i = 0; i < cache.line_num; ++i) {
+    line_t* line = &set->lines[i];
+
+    if (!line->valid) { continue; }
+    if (line->tag != tag) { continue; }
+
+    ++hits;
+    update(set, i);
+    return;
+  }
+  ++misses;
+
+  for (size_t i = 0; i < cache.line_num; ++i) {
+    line_t* line = &set->lines[i];
+
+    if (line->valid) { continue; }
+
+    line->valid = true;
+    line->tag = tag;
+    update(set, i);
+    return;
+  }
+
+  ++evictions;
+
+  for (size_t i = 0; i < cache.line_num; ++i) {
+    line_t* line = &set->lines[i];
+
+    if (line->timestamp) { continue; }
+
+    line->valid = true;
+    line->tag = tag;
+    update(set, i);
+    return;
+  }
+}
+
+
+void update(set_t *set, size_t line_no) {
+  line_t *line = &set->lines[line_no];
+
+  for (size_t i = 0; i < cache.line_num; ++i) {
+    line_t *it = &set->lines[i];
+    if (!it->valid) { continue; }
+    if (it->timestamp <= line->timestamp) { continue; }
+
+    --it->timestamp;
+  }
+
+  line->timestamp = cache.line_num - 1;
 }
--- trans.c
+++ trans.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * trans.c - Matrix transpose B = A^T
  *
  * Each transpose function must have a prototype of the form:
@@ -6,7 +6,7 @@
  *
  * A transpose function is evaluated by counting the number of misses
  * on a 1KB direct mapped cache with a block size of 32 bytes.
- */ 
+ */
 #include <stdio.h>
 #include "cachelab.h"
 
@@ -20,21 +20,157 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
  *     be graded. 
  */
 char transpose_submit_desc[] = "Transpose submission";
-void transpose_submit(int M, int N, int A[N][M], int B[M][N])
-{
+
+void transpose_submit(int M, int N, int A[N][M], int B[M][N]);
+
+void transpose_64_64(int M, int N, int A[N][M], int B[M][N]);
+
+void transpose_32_32(int M, int N, int A[N][M], int B[M][N]);
+
+void transpose_61_67(int M, int N, int A[N][M], int B[M][N]);
+
+void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
+    if (M == 32) {
+        transpose_32_32(M, N, A, B);
+    } else if (M == 64) {
+        transpose_64_64(M, N, A, B);
+    } else {
+        transpose_61_67(M, N, A, B);
+    }
+}
+
+void transpose_32_32(int M, int N, int A[N][M], int B[M][N]) {
+    const int bSize = 8;
+    int colBlock, rowBlock, r, c, temp = 0, d = 0;
+    for (colBlock = 0; colBlock < N; colBlock += bSize) {
+        for (rowBlock = 0; rowBlock < N; rowBlock += bSize) {
+            for (r = rowBlock; r < rowBlock + bSize; r++) {
+                for (c = colBlock; c < colBlock + bSize; c++) {
+                    if (r != c) {
+                        B[c][r] = A[r][c];
+                    } else {
+                        temp = A[r][c];
+                        d = r;
+                    }
+                }
+                if (rowBlock == colBlock) {
+                    B[d][d] = temp;
+                }
+            }
+        }
+    }
+}
+
+void transpose_64_64(int M, int N, int A[N][M], int B[M][N]) {
+    const int bSize = 8;
+    int colRun, rowRun, k, a0, a1, a2, a3, a4, a5, a6, a7;
+
+    for (colRun = 0; colRun < N; colRun += bSize) {
+        for (rowRun = 0; rowRun < M; rowRun += bSize) {
+
+            for (k = 0; k < 4; k++) {
+                a0 = A[colRun + k][rowRun + 0];
+                a1 = A[colRun + k][rowRun + 1];
+                a2 = A[colRun + k][rowRun + 2];
+                a3 = A[colRun + k][rowRun + 3];
+                a4 = A[colRun + k][rowRun + 4];
+                a5 = A[colRun + k][rowRun + 5];
+                a6 = A[colRun + k][rowRun + 6];
+                a7 = A[colRun + k][rowRun + 7];
+
+                B[rowRun + 0][colRun + k + 0] = a0;
+                B[rowRun + 0][colRun + k + 4] = a5;
+                B[rowRun + 1][colRun + k + 0] = a1;
+                B[rowRun + 1][colRun + k + 4] = a6;
+                B[rowRun + 2][colRun + k + 0] = a2;
+                B[rowRun + 2][colRun + k + 4] = a7;
+                B[rowRun + 3][colRun + k + 0] = a3;
+                B[rowRun + 3][colRun + k + 4] = a4;
+            }
+
+            a0 = A[colRun + 4][rowRun + 4];
+            a1 = A[colRun + 5][rowRun + 4];
+            a2 = A[colRun + 6][rowRun + 4];
+            a3 = A[colRun + 7][rowRun + 4];
+            a4 = A[colRun + 4][rowRun + 3];
+            a5 = A[colRun + 5][rowRun + 3];
+            a6 = A[colRun + 6][rowRun + 3];
+            a7 = A[colRun + 7][rowRun + 3];
+
+            B[rowRun + 4][colRun + 0] = B[rowRun + 3][colRun + 4];
+            B[rowRun + 4][colRun + 4] = a0;
+            B[rowRun + 3][colRun + 4] = a4;
+            B[rowRun + 4][colRun + 1] = B[rowRun + 3][colRun + 5];
+            B[rowRun + 4][colRun + 5] = a1;
+            B[rowRun + 3][colRun + 5] = a5;
+            B[rowRun + 4][colRun + 2] = B[rowRun + 3][colRun + 6];
+            B[rowRun + 4][colRun + 6] = a2;
+            B[rowRun + 3][colRun + 6] = a6;
+            B[rowRun + 4][colRun + 3] = B[rowRun + 3][colRun + 7];
+            B[rowRun + 4][colRun + 7] = a3;
+            B[rowRun + 3][colRun + 7] = a7;
+            for (k = 0; k < 3; k++) {
+                a0 = A[colRun + 4][rowRun + 5 + k];
+                a1 = A[colRun + 5][rowRun + 5 + k];
+                a2 = A[colRun + 6][rowRun + 5 + k];
+                a3 = A[colRun + 7][rowRun + 5 + k];
+                a4 = A[colRun + 4][rowRun + k];
+                a5 = A[colRun + 5][rowRun + k];
+                a6 = A[colRun + 6][rowRun + k];
+                a7 = A[colRun + 7][rowRun + k];
+
+                B[rowRun + 5 + k][colRun + 0] = B[rowRun + k][colRun + 4];
+                B[rowRun + 5 + k][colRun + 4] = a0;
+                B[rowRun + k][colRun + 4] = a4;
+                B[rowRun + 5 + k][colRun + 1] = B[rowRun + k][colRun + 5];
+                B[rowRun + 5 + k][colRun + 5] = a1;
+                B[rowRun + k][colRun + 5] = a5;
+                B[rowRun + 5 + k][colRun + 2] = B[rowRun + k][colRun + 6];
+                B[rowRun + 5 + k][colRun + 6] = a2;
+                B[rowRun + k][colRun + 6] = a6;
+                B[rowRun + 5 + k][colRun + 3] = B[rowRun + k][colRun + 7];
+                B[rowRun + 5 + k][colRun + 7] = a3;
+                B[rowRun + k][colRun + 7] = a7;
+            }
+        }
+    }
+}
+
+void transpose_61_67(int M, int N, int A[N][M], int B[M][N]) {
+    const int bSize = 16;
+    int colBlock, rowBlock, r, c, temp, d = 0;
+
+    for (colBlock = 0; colBlock < M; colBlock += bSize) {
+        for (rowBlock = 0; rowBlock < N; rowBlock += bSize) {
+            for (r = rowBlock; (r < N) && (r < rowBlock + bSize); r++) {
+                for (c = colBlock; (c < M) && (c < colBlock + bSize); c++) {
+                    if (r != c) {
+                        B[c][r] = A[r][c];
+                    } else {
+                        temp = A[r][c];
+                        d = r;
+                    }
+                }
+
+                if (rowBlock == colBlock) {
+                    B[d][d] = temp;
+                }
+            }
+        }
+    }
 }
 
 /* 
  * You can define additional transpose functions below. We've defined
  * a simple one below to help you get started. 
- */ 
+ */
 
 /* 
  * trans - A simple baseline transpose function, not optimized for the cache.
  */
 char trans_desc[] = "Simple row-wise scan transpose";
-void trans(int M, int N, int A[N][M], int B[M][N])
-{
+
+void trans(int M, int N, int A[N][M], int B[M][N]) {
     int i, j, tmp;
 
     for (i = 0; i < N; i++) {
@@ -42,8 +178,7 @@ void trans(int M, int N, int A[N][M], int B[M][N])
             tmp = A[i][j];
             B[j][i] = tmp;
         }
-    }    
-
+    }
 }
 
 /*
@@ -53,14 +188,12 @@ void trans(int M, int N, int A[N][M], int B[M][N])
  *     performance. This is a handy way to experiment with different
  *     transpose strategies.
  */
-void registerFunctions()
-{
+void registerFunctions() {
     /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+    registerTransFunction(transpose_submit, transpose_submit_desc);
 
     /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
-
+    registerTransFunction(trans, trans_desc);
 }
 
 /* 
@@ -68,8 +201,7 @@ void registerFunctions()
  *     A. You can check the correctness of your transpose by calling
  *     it before returning from the transpose function.
  */
-int is_transpose(int M, int N, int A[N][M], int B[M][N])
-{
+int is_transpose(int M, int N, int A[N][M], int B[M][N]) {
     int i, j;
 
     for (i = 0; i < N; i++) {
@@ -81,4 +213,3 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
     }
     return 1;
 }
-
